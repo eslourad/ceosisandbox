3c99e17f330df726d49f8f638efd7e84cda62b36
CarLKnight6 <sancheja123@gmail.com>
2022-10-19 14:00:49 +0800

    carl riverpod latest

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:sandbox/examples/test_screen.dart';
+import 'package:sandbox/examples/carl/home_screen.dart';
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       // home: const MainScreen(),
-      home: const TestScreen(),
+      home: const HomeScreen2(),
     );
   }
 }
