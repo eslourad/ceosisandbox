c5149a81b879ea8d3f6a4ce3e94dc9aa378a56bd
123Lance-Olana <lance.ceosi@gmail.com>
2022-10-21 06:42:27 +0800

    Fix. consumer widget

--- lib/examples/Lance/screens/my_home_page.dart
+++ lib/examples/Lance/screens/my_home_page.dart
@@ -4,20 +4,21 @@ import 'package:sandbox/examples/Lance/providers/providers.dart';
 
 import '../model/post_model.dart';
 
-class MyHomePage extends ConsumerWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    var futureProvider =
-        ref.watch(postFutureProvider(ref.read(getPostProvider.notifier).state));
-
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(ref.read(getPostProvider.notifier).state),
+        title: const Text(
+          'Home',
+          style: TextStyle(
+              color: Colors.white, fontWeight: FontWeight.bold, fontSize: 18),
+        ),
         centerTitle: true,
       ),
-      body: Consumer(futureProvider: futureProvider),
+      body: const Consumer(),
     );
   }
 }
@@ -25,62 +26,95 @@ class MyHomePage extends ConsumerWidget {
 class Consumer extends ConsumerWidget {
   const Consumer({
     Key? key,
-    required this.futureProvider,
   }) : super(key: key);
-
-  final AsyncValue<List<PostLance>?> futureProvider;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    var futureProvider =
+        ref.watch(postFutureProvider(ref.read(getPostProvider.notifier).state));
     return SizedBox(
       child: futureProvider.when(
         data: ((data) {
           return Column(
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  MaterialButton(
-                      color: Colors.blue,
-                      onPressed: (() {
-                        ref.read(getPostProvider.notifier).all();
+              Padding(
+                padding: const EdgeInsets.only(left: 10, right: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    MaterialButton(
+                        color: Colors.blue,
+                        onPressed: (() {
+                          ref.read(getPostProvider.notifier).all();
 
-                        Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const MyHomePage(),
-                            ));
-                      }),
-                      child: const Text('All')),
-                  MaterialButton(
-                      color: Colors.blue,
-                      child: const Text('1'),
-                      onPressed: () {
-                        ref.read(getPostProvider.notifier).userId1();
+                          Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const MyHomePage(),
+                              ));
+                        }),
+                        child: const Text(
+                          'All',
+                          style: TextStyle(color: Colors.white),
+                        )),
+                    MaterialButton(
+                        color: Colors.blue,
+                        child: const Text(
+                          '1',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        onPressed: () {
+                          ref.read(getPostProvider.notifier).userId1();
 
-                        Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const MyHomePage(),
-                            ));
-                      }),
-                  MaterialButton(
-                      color: Colors.blue,
-                      child: const Text('2'),
-                      onPressed: () {
-                        ref.read(getPostProvider.notifier).userId2();
+                          Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const MyHomePage(),
+                              ));
+                        }),
+                    MaterialButton(
+                        color: Colors.blue,
+                        child: const Text(
+                          '2',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        onPressed: () {
+                          ref.read(getPostProvider.notifier).userId2();
 
-                        Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const MyHomePage(),
-                            ));
-                      }),
-                ],
+                          Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const MyHomePage(),
+                              ));
+                        }),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Text(
+                'Posts from : ${ref.read(getPostProvider.notifier).state}',
+                style: const TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18),
+              ),
+              const SizedBox(
+                height: 20,
               ),
               Expanded(
                 child: SizedBox(
-                  child: ListView.builder(
+                  child: ListView.separated(
+                      separatorBuilder: ((context, index) {
+                        return const Padding(
+                          padding: EdgeInsets.only(left: 20, right: 20),
+                          child: Divider(
+                            color: Colors.blue,
+                            thickness: 2,
+                          ),
+                        );
+                      }),
                       itemCount: data!.length,
                       itemBuilder: ((context, index) {
                         return ListTile(
@@ -97,8 +131,20 @@ class Consumer extends ConsumerWidget {
                             print(data[index].userId);
                             print(data[index].id);
                           },
-                          title: Text(data[index].title!),
-                          subtitle: Text(data[index].body!),
+                          title: Text(
+                            data[index].title!,
+                            style: const TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 22),
+                          ),
+                          subtitle: Text(
+                            data[index].body!,
+                            style: const TextStyle(
+                                color: Colors.grey,
+                                fontWeight: FontWeight.normal,
+                                fontSize: 14),
+                          ),
                         );
                       })),
                 ),
