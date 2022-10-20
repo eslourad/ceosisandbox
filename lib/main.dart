2b9d37181b9861baf83319ec0bf81b12d160bc14
CarLKnight6 <sancheja123@gmail.com>
2022-10-20 13:45:54 +0800

    fix some error

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:sandbox/examples/carl/home_screen.dart';
+
+import 'package:sandbox/examples/carl/main_screen.dart';
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       // home: const MainScreen(),
-      home: const HomeScreen2(),
+      home: const MainScreen2(),
     );
   }
 }
