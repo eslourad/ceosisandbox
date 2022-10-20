13a22de1539b3e64ce0d8f7c8eec2eab6dc6c8ed
CarLKnight6 <sancheja123@gmail.com>
2022-10-21 05:58:34 +0800

    merging

--- lib/examples/carl/Models/model.dart
+++ lib/examples/carl/Models/model.dart
@@ -37,5 +37,3 @@ class Post {
         "body": body,
       };
 }
-
-
---
+++ lib/examples/carl/screens/searchpost_screen.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sandbox/examples/carl/Providers/providers.dart';
+import '../Models/model.dart';
+
+final thirdNumberProvider = StateProvider((_) => 3);
+
+class SearchPostScreen extends ConsumerWidget {
+  SearchPostScreen(this.userId, {super.key});
+  String userId;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    print("TEST 3 I am being rebuild");
+
+    final watchedsearchpostprovider =
+        ref.watch(SearchpostFutureProvider(userId));
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('simple riverpod fetch api'),
+        ),
+        body: watchedsearchpostprovider.when(
+          data: ((watchedsearchpostprovider) {
+            List<Post> post = watchedsearchpostprovider;
+            return Column(
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                      itemCount: post.length,
+                      itemBuilder: (_, index) {
+                        return InkWell(
+                          onTap: () {},
+                          child: Card(
+                            color: Colors.blue,
+                            elevation: 4,
+                            margin: const EdgeInsets.symmetric(vertical: 10),
+                            child: ListTile(
+                              title: Text(
+                                  'ID:${post[index].id.toString()} ${post[index].title}'),
+                              subtitle: Text(post[index].body),
+                              trailing: Text(
+                                  'userID:${post[index].userId.toString()}'),
+                            ),
+                          ),
+                        );
+                      }),
+                ),
+              ],
+            );
+          }),
+          error: ((error, stackTrace) {
+            return const Text('error');
+          }),
+          loading: (() {
+            return const Center(child: CircularProgressIndicator());
+          }),
+        ));
+  }
+}
