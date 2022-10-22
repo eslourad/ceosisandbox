f649999747851633dc4e3d77a3d3418b53ef791a
BiscochoChloie <chloiebiscocho123@gmail.com>
2022-10-22 14:37:45 +0800

    unfinished

--- lib/examples/Chloie/data_providers.dart
+++ lib/examples/Chloie/data_providers.dart
@@ -1,10 +1,12 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:sandbox/examples/Chloie/models/photoModel.dart';
 import 'package:sandbox/examples/Chloie/notifier.dart';
-import 'package:sandbox/examples/Chloie/screens/photo_list_screen.dart';
+import 'repositories/photo_repository.dart';
+
+final photoRepositoryProvider = StateProvider((_) => PhotoRepository());
 
 final photoDataProvider = FutureProvider<List<PhotoModel>>((ref) async {
-  return ref.read(photoRepositoryProvider).getAllPhotos();
+  return ref.read(photoRepositoryProvider).getAllPhotos("");
 });
 
 final singlePhotoDataProvider =
@@ -13,19 +15,16 @@ final singlePhotoDataProvider =
     return ref.read(photoRepositoryProvider).getPhoto(id.toString());
   },
 );
-final filterPhotoDataProvider =
-    FutureProvider.family.autoDispose<List<PhotoModel>, String>(
-  (ref, albumId) async {
-    return ref
-        .watch(photoRepositoryProvider)
-        .getFilterPhoto(albumId.toString());
+
+final filteredPhotoDataProvider =
+    FutureProvider.family.autoDispose<List<PhotoModel>, int>(
+  (ref, id) async {
+    return ref.read(photoRepositoryProvider).getAllPhotos(id.toString());
   },
 );
-// final filterPhotoProvider = StateNotifierProvider.autoDispose<PhotosNotifier,
-//     AsyncValue<List<PhotoModel>>>((ref) {
-//   return PhotosNotifier();
-// });
-final filterPhotoProvider =
-    StateNotifierProvider<PhotosFilterNotifier, String>((ref) {
-  return PhotosFilterNotifier();
+
+final listPhotoProvider =
+    StateNotifierProvider<PhotoListNotifier, AsyncValue<List<PhotoModel>>>(
+        (ref) {
+  return PhotoListNotifier(ref);
 });
--- lib/examples/Chloie/notifier.dart
+++ lib/examples/Chloie/notifier.dart
@@ -1,18 +1,33 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:sandbox/examples/Chloie/models/photoModel.dart';
+import 'package:sandbox/examples/Chloie/repositories/photo_repository.dart';
 
-class PhotosFilterNotifier extends StateNotifier<String> {
-  PhotosFilterNotifier() : super('');
+class PhotoListNotifier extends StateNotifier<AsyncValue<List<PhotoModel>>> {
+  final Ref ref;
+  PhotoListNotifier(
+    this.ref, [
+    AsyncValue<List<PhotoModel>>? photo,
+  ]) : super(photo ?? const AsyncValue.loading()) {
+    void getPhotos() async {
+      final photo = await PhotoRepository().getAllPhotos("");
+      state = AsyncValue.data(photo);
+    }
 
-  void photofilter(String userid) {
-    state = userid;
+    void filterId(String id) async {
+      String albumId = id == '' ? id : '?albumId=$id';
+
+      final photo = await PhotoRepository().getAllPhotos(albumId);
+      state = AsyncValue.data(photo);
+    }
   }
 }
 
-class PhotosListNotifier extends StateNotifier<AsyncValue<List<PhotoModel>>> {
-  PhotosListNotifier() : super(const AsyncValue.data(<PhotoModel>[]));
-
-  getListPhotos(String photoList) {
-    state = photoList as AsyncValue<List<PhotoModel>>;
+class PhotosFilterNotifier extends StateNotifier<AsyncValue<List<PhotoModel>>> {
+  PhotosFilterNotifier(this.ref) : super(const AsyncValue.data(<PhotoModel>[]));
+  final Ref ref;
+  void photofilter(String id) async {
+    String albumId = id == '' ? id : '?albumId=$id';
+    final photo = await PhotoRepository().getAllPhotos(albumId);
+    state = AsyncValue.data(photo);
   }
 }
