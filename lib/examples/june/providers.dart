4e4d036d78167b80a834977b1617100a63e3c6fc
June Paolo Tabequero <62154256+iiamjune@users.noreply.github.com>
2022-10-22 14:40:30 +0800

    state notifier provider progress

--- lib/examples/june/providers.dart
+++ lib/examples/june/providers.dart
@@ -4,31 +4,38 @@ import 'package:sandbox/examples/june/models/post_model.dart';
 import 'package:sandbox/examples/june/notifiers/post_list_notifier.dart';
 import 'package:sandbox/examples/june/repositories/post_repository.dart';
 
+//Post Repository Provider
 final postRepositoryProvider =
     Provider<PostRepository>((ref) => PostRepository());
-final postsFutureProvider =
-    FutureProvider.family.autoDispose<List<PostListModel>?, String>(
-  (ref, query) async {
-    return ref.watch(postRepositoryProvider).getPosts(query: query);
+//Future Provider for all posts
+final postsFutureProvider = FutureProvider.autoDispose<List<PostListModel>?>(
+  (ref) async {
+    return ref
+        .watch(postRepositoryProvider)
+        .getPosts(query: ref.watch(postsFilterProvider));
   },
 );
+// Future Provider for single post
 final postFutureProvider =
     FutureProvider.family.autoDispose<PostModel?, String>(
   (ref, postID) async {
     return ref.watch(postRepositoryProvider).getPost(postID);
   },
 );
+// State Provider for single post
 final postStateProvider = StateProvider<PostModel>(
   (ref) {
     return PostModel();
   },
 );
+// State Notifier Provider for post list filter
 final postsFilterProvider =
     StateNotifierProvider.autoDispose<PostListFilterNotifier, String>(
   (ref) {
     return PostListFilterNotifier();
   },
 );
+// State Notifier Provider for filtered post list
 final filteredPostsProvider = StateNotifierProvider.autoDispose<
     FilteredPostsNotifier, AsyncValue<List<PostListModel>?>>(
   (ref) {
